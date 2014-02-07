747a809d113ed461c1f1200c4dd5a7908b9b38e1
Alex <apeggs2000@users.noreply.github.com>
2014-02-07 10:15:07 -0800

    Create Library-of-Pong.js

---
+++ Library-of-Pong.js
@@ -0,0 +1,9 @@
+/* 
+* 
+* Library-of-Pong.js is a lightweight game engine designed to take a good part of the work out of creating Pong and Pong-variant games
+* 
+* Courtesy of WebWonders
+* 
+* Code by apeggs2000
+* 
+*/
